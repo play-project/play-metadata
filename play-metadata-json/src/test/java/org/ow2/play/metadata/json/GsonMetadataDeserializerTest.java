081ed89fe4a27dc9c05cbd197edc2681548193cb
Christophe Hamerling <christophe.hamerling@gmail.com>
2012-07-13 17:23:08 +0200

    More tests

--- play-metadata-json/src/test/java/org/ow2/play/metadata/json/GsonMetadataDeserializerTest.java
+++ play-metadata-json/src/test/java/org/ow2/play/metadata/json/GsonMetadataDeserializerTest.java
@@ -23,7 +23,9 @@ import java.util.List;
 
 import junit.framework.TestCase;
 
+import org.ow2.play.metadata.api.Data;
 import org.ow2.play.metadata.api.MetaResource;
+import org.ow2.play.metadata.api.Metadata;
 
 /**
  * @author chamerling
@@ -38,6 +40,39 @@ public class GsonMetadataDeserializerTest extends TestCase {
 						.getResourceAsStream("/metadata-1resource.rdf.json"));
 
 		assertEquals(1, result.size());
+
+		MetaResource mr = result.get(0);
+		System.out.println(mr);
+
+		assertEquals("stream", mr.getResource().getName());
+		assertEquals(
+				"http://streams.event-processing.org/ids/FacebookStatusFeed",
+				mr.getResource().getUrl());
+
+		assertEquals(5, mr.getMetadata().size());
+
+		List<Metadata> list = mr.getMetadata();
+
+		Metadata meta = new Metadata("http://purl.org/dc/elements/1.1/title",
+				new Data("literal", "Facebook Status Feed"));
+		assertTrue(list.contains(meta));
+		
+		meta = new Metadata("http://purl.org/dc/elements/1.1/description",
+				new Data("literal", "A stream of Facebook Wall updates."));
+		assertTrue(list.contains(meta));
+
+		meta = new Metadata("http://www.w3.org/2002/06/xhtml2/icon",
+				new Data("uri", "http://s-static.ak.facebook.com/rsrc.php/yi/r/q9U99v3_saj.ico"));
+		assertTrue(list.contains(meta));
+
+		meta = new Metadata("http://www.play-project.eu/xml/ns/topic",
+				new Data("uri", "http://streams.event-processing.org/ids/FacebookStatusFeed"));
+		assertTrue(list.contains(meta));
+
+		meta = new Metadata("http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+				new Data("uri", "http://events.event-processing.org/types/Stream"));
+		assertTrue(list.contains(meta));
+
 	}
 
 	public void testDeserializeFromResource2() throws Exception {
